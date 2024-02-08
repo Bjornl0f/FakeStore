112cbe60c4c63e6945f0bcfb487e0e09f01f9012
Stefan Kostyk <kostykstefan@gmail.com>
2024-02-08 19:54:09 +0200

    Changed the default route link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [{
   component: HomeComponent
 },
 {
-  path: '', redirectTo: 'home', pathMatch: 'full'
+  path: '', redirectTo: 'products', pathMatch: 'full'
 },
 {
   path: 'products',
